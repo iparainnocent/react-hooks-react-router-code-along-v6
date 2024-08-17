9d236442c54d4137b6bce33fb35fbb3aa35b97c4
innocent <iparainnocent@gmail.com>
2024-08-17 10:40:38 +0300

    commit changes

---
+++ src/components/NavBar.js
@@ -0,0 +1,20 @@
+import { NavLink } from "react-router-dom";
+import "./NavBar.css";
+
+const NavBar = () => {
+  return (
+    <nav id="navbar">
+      <NavLink to="/" className="nav-link">
+        Home
+      </NavLink>
+      <NavLink to="/about" className="nav-link">
+        About
+      </NavLink>
+      <NavLink to="/login" className="nav-link">
+        Login
+      </NavLink>
+    </nav>
+  );
+};
+
+export default NavBar;
